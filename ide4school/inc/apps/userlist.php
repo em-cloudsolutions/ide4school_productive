d55753caa014e6e4f8f6b8b8f35136312f25fca8
em-cloudsolutions <107758318+em-cloudsolutions@users.noreply.github.com>
2024-01-05 10:06:29 +0100

    Update userlist.php

--- ide4school/inc/apps/userlist.php
+++ ide4school/inc/apps/userlist.php
@@ -129,16 +129,18 @@ if(isset($_POST["importUsers"]))
 
 
         $Reader->ChangeSheet($i);
-
-        function randomPassword() {
-            $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-            $pass = array(); //remember to declare $pass as an array
-            $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-            for ($i = 0; $i < 8; $i++) {
-                $n = rand(0, $alphaLength);
-                $pass[] = $alphabet[$n];
+            
+        if (!function_exists('randomPassword')){
+            function randomPassword() {
+                $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+                $pass = array(); //remember to declare $pass as an array
+                $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+                for ($i = 0; $i < 8; $i++) {
+                    $n = rand(0, $alphaLength);
+                    $pass[] = $alphabet[$n];
+                }
+                return implode($pass); //turn the array into a string
             }
-            return implode($pass); //turn the array into a string
         }
 
         $request_header = '
